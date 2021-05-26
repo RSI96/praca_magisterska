31804e3d93b508947d884baeffe7dcafa4f14320
radek.sikora <radek.sikora@bitcave.eu>
2021-05-26 17:57:02 +0200

    save file

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+
+#UPLOAD_FOLDER = ''
+
 
 app = Flask(__name__)
 app.config.from_object("src.config.Config")
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 db = SQLAlchemy(app)
 
 CORS(app)
-
-
 
 class User(db.Model):
     __tablename__ = "users"
@@ -22,11 +27,44 @@ class User(db.Model):
         self.email = email
 
 
-
+@app.route("/", methods=['GET'])
+def home():
+    return {
+        'hello': 'world'
+    }
 
 @app.route("/testjson", methods=['GET'])
-
-def hello_world():
+def testjson():
     return {
         'test': 'json'
     }
+
+
+@app.route('/addFile', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
+'''
+@app.route('/addFile', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file (file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return 'file uploaded successfully'
+'''
