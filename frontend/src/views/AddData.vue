5ee5dc2e9a9618e3cf196067cbb47c1a44d86f01
radek.sikora <radek.sikora@bitcave.eu>
2021-05-26 16:22:52 +0200

    upload file func

--- frontend/src/views/AddData.vue
+++ frontend/src/views/AddData.vue
@@ -1,12 +1,53 @@
 <template>
-    <div class="page">
-        <h2>Add .csv files here</h2>
-
+  <div class="page">
+    <h2>Add .csv files here</h2>
+    <div class="large-12 medium-12 small-12 cell">
+      <label
+        >File
+        <input
+          type="file"
+          id="file"
+          ref="file"
+          v-on:change="handleFileUpload()"
+        />
+      </label>
+      <button v-on:click="submitFile()">Submit</button>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'addData',
-    }
+import axios from 'axios';
+//import { HTTP } from "../http-common";
+
+export default {
+  name: "addData",
+  data() {
+    return {
+      file: "",
+    };
+  },
+  methods: {
+    handleFileUpload() {
+      this.file = this.$refs.file.files[0];
+    },
+    submitFile() {
+      let formData = new FormData();
+      formData.append("file", this.file);
+
+      axios
+        .post("http://localhost:5000/addFile", formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then(function () {
+          console.log("SUCCESS!!");
+        })
+        .catch(function () {
+          console.log("FAILURE!!");
+        });
+    },
+  },
+};
 </script>
